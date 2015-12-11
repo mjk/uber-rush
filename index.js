395f4a0abefccee29b701d26172293f82f18aa0a
mjk <keesan@gmail.com>
2015-12-11 17:38:35 -0500

    Added more configuration options.

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ module.exports = {
     init: function(options) {
         if (!options.client_secret) throw new Error("client_secret must be provided");
         if (!options.client_id) throw new Error("client_id must be provided");
+        
+        var sandbox = options.sandbox || !options.production;
 
         nconf.use('memory');
 
         nconf.set('uber_api_client_secret', options.client_secret);
         nconf.set('uber_api_client_id', options.client_id);
         nconf.set('uber_api_server_token', options.server_token);
+        nconf.set('uber_api_sandbox', sandbox);
+        nconf.set('uber_api_sandbox', sandbox);
 
         return API.getToken();
     },
