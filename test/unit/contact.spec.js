c5eeae40928946c898c84b160f7527b5411c3a4e
mjk <keesan@gmail.com>
2016-10-03 21:33:52 -0400

    Only throwing errors in strict contact mode)

--- test/unit/contact.spec.js
+++ test/unit/contact.spec.js
@@ -14,25 +14,31 @@ describe('Contacts', function() {
 
   it('Contact should accept a variety of American numbers', function() {
     assert.doesNotThrow(() => {
-      new rush.Contact(deliveryConfig.pickup.contact);
-      new rush.Contact(deliveryConfig.dropoff.contact);
-      new rush.Contact({phone:{number: '844-555-5555'}});
-      new rush.Contact({phone:{number: '855-555-5555'}});
-      new rush.Contact({phone:{number: '900-555-5555'}});
-      new rush.Contact({phone:{number: '800-555-5555'}});
-      new rush.Contact({phone:{number: '716-555-5555'}});
-      new rush.Contact({phone:{number: '+1 212 555 1212'}});
-      new rush.Contact({phone:{number: '+1 844 555 1212'}});
+      new rush.Contact(Object.assign(deliveryConfig.pickup.contact, {strict: 1}));
+      new rush.Contact(Object.assign(deliveryConfig.dropoff.contact, {strict: 1}));
+      new rush.Contact({strict: 1, phone:{number: '844-555-5555'}});
+      new rush.Contact({strict: 1, phone:{number: '855-555-5555'}});
+      new rush.Contact({strict: 1, phone:{number: '900-555-5555'}});
+      new rush.Contact({strict: 1, phone:{number: '800-555-5555'}});
+      new rush.Contact({strict: 1, phone:{number: '716-555-5555'}});
+      new rush.Contact({strict: 1, phone:{number: '+1 212 555 1212'}});
+      new rush.Contact({strict: 1, phone:{number: '+1 844 555 1212'}});
     });
   });
 
   it('Contact should not accept invalid numbers', function() {
     assert.throws(() => {
-      new rush.Contact({phone:{number: '555 1212'}});
+      new rush.Contact({strict: 1, phone:{number: '555 1212'}});
     });
     assert.throws(() => {
-      new rush.Contact({phone:{number: '1 555 12'}});
+      new rush.Contact({strict: 1, phone:{number: '1 555 12'}});
     });
   });
 
+  it('Contact should not throw invalid number errors outside of strict mode', function() {
+    assert.doesNotThrow(() => {
+      new rush.Contact({phone:{number: '555 1212'}});
+      new rush.Contact({phone:{number: '1 555 12'}});
+    });
+  });
 })
